25c3bcc3ea43c7a584eecb7c79ce50f8a29cdd5d
MaureenDadap <maureendadap@gmail.com>
2021-11-02 16:03:52 +0800

    validations for admin restaurant show form only when resto is not per admin is not created yet

--- myrestaurant.php
+++ myrestaurant.php
@@ -3,17 +3,32 @@ include('utils/session_m.php');
 include("common/head_scripts.php");
 include("common/components.php");
 
-$error = "";
-$response = "";
-
-
 if (!isset($login_session)) {
   header('Location: managerlogin.php'); // Redirecting To Home Page
 }
 
 $error = "";
 $response = "";
+$m_id = $_SESSION['login_user1'];
+$hasRestaurant = false;
 
+//check first if manager has already created a restaurant for themselves
+$query = "SELECT * FROM restaurants WHERE m_id = '$m_id'";
+$result = $conn->query($query);
+if (mysqli_num_rows($result) > 0) {
+  $hasRestaurant = true;
+  $restaurant = mysqli_fetch_assoc($result);
+  $r_id = $restaurant['R_ID'];
+
+  //check for the total number of menu items in restaurant
+  $query = "SELECT COUNT FROM food WHERE r_id = '$r_id'";
+  $menu_items = $conn->query($query);
+  if (!$menu_items) {
+    $menu_items = 0;
+  }
+}
+
+// creation of new restaurant
 if (isset($_POST['submit'])) {
   $name = $conn->real_escape_string($_POST['name']);
   $email = $conn->real_escape_string($_POST['email']);
@@ -30,6 +45,7 @@ if (isset($_POST['submit'])) {
   }
 }
 ?>
+
 <!DOCTYPE html>
 <html>
 <?= head("My Restaurant") ?>
@@ -40,6 +56,7 @@ if (isset($_POST['submit'])) {
   navbar("control");
   ?>
   <main>
+    <!-- ======= Creating Restaurant Success Modal ======= -->
     <div class="modal fade" id="successModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
       <div class="modal-dialog">
         <div class="modal-content">
@@ -55,42 +72,75 @@ if (isset($_POST['submit'])) {
         </div>
       </div>
     </div>
+    <!-- End success modal -->
 
     <div class="container">
       <div class="row">
+        <!-- ======= Admin sidebar ======= -->
         <div class="col-md-4 text-center">
           <?= adminSideBar("restaurant"); ?>
         </div>
+        <!-- End admin sidebar -->
+
+        <!-- ======= Restaurant Info ======= -->
         <div class="col-md-8">
           <div class="form-area px-lg-5 mx-lg-5">
-            <form action="" method="POST">
-              <br style="clear: both">
-              <h3 class="text-center mb-4">MY RESTAURANT</h3>
+            <h3 class="text-center mb-4">MY RESTAURANT</h3>
+            <?php if ($hasRestaurant === true) { ?>
+              <table class="table table-striped">
+                <tbody>
+                  <tr>
+                    <td><strong>Restaurant Name</strong></td>
+                    <td><?php echo $restaurant['name']; ?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Restaurant Email</strong></td>
+                    <td><?php echo $restaurant['email']; ?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Restaurant Contact</strong></td>
+                    <td><?php echo $restaurant['contact']; ?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Restaurant Address: </strong> </td>
+                    <td><?php echo $restaurant['address']; ?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Restaurant Total Menu Items: </strong></td>
+                    <td><?php echo $menu_items; ?></td>
+                  </tr>
+                </tbody>
+              </table>
+            <?php } else { ?>
+              <form action="" method="POST">
+                <br style="clear: both">
 
-              <div class="form-group mb-3">
-                <input type="text" class="form-control" id="name" name="name" placeholder="Your Restaurant's Name" required="">
-              </div>
+                <div class="form-group mb-3">
+                  <input type="text" class="form-control" id="name" name="name" placeholder="Your Restaurant's Name" required="">
+                </div>
 
-              <div class="form-group mb-3">
-                <input type="email" class="form-control" id="email" name="email" placeholder="Your Restaurant's Email" required="">
-              </div>
+                <div class="form-group mb-3">
+                  <input type="email" class="form-control" id="email" name="email" placeholder="Your Restaurant's Email" required="">
+                </div>
 
-              <div class="form-group mb-3">
-                <input type="text" class="form-control" id="contact" name="contact" placeholder="Your Restaurant's Contact Number" required="">
-              </div>
+                <div class="form-group mb-3">
+                  <input type="text" class="form-control" id="contact" name="contact" placeholder="Your Restaurant's Contact Number" required="">
+                </div>
 
-              <div class="form-group mb-3">
-                <input type="text" class="form-control" id="address" name="address" placeholder="Your Restaurant's Address" required="">
-              </div>
+                <div class="form-group mb-3">
+                  <input type="text" class="form-control" id="address" name="address" placeholder="Your Restaurant's Address" required="">
+                </div>
 
-              <label class="text-danger mb-3"><span> <?php echo $error;  ?> </span></label>
+                <label class="text-danger mb-3"><span> <?php echo $error;  ?> </span></label>
 
-              <div class="form-group mb-3">
-                <button type="submit" id="submit" name="submit" class="btn btn-primary pull-right"> ADD RESTAURANT </button>
-              </div>
-            </form>
+                <div class="form-group mb-3">
+                  <button type="submit" id="submit" name="submit" class="btn btn-primary pull-right"> ADD RESTAURANT </button>
+                </div>
+              </form>
+            <?php } ?>
           </div>
         </div>
+        <!-- End restaurant info -->
       </div>
     </div>
   </main>
